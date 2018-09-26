0895e06b860d74670501b34817aa6796bec9a756
Nar Kumar Chhantyal <chtn@viessmann.com>
2018-09-26 12:20:23 +0200

    add some docs

--- wordcount.py
+++ wordcount.py
@@ -13,8 +13,16 @@ class Parser:
         self.loggly_key = loggly_key
 
     def word_split(self, x):
+        """
+        - Method/function which runs on Spark executor
+        - Log some useful information
+        - Use regex to split only words(not special characters)
+        :param x: Spark rdd or tuple with (text, line_num)
+        :return: words
+        """
         words = re.split('\W+', x[0])
         length = len(words)
+        # configures logger in executors on first run
         logger = get_configured_logger(self.loggly_key)
         logger.info("Line {} contains {} words.".format(x[1], length))
         # filter empty values
@@ -22,16 +30,22 @@ class Parser:
 
 
 def main(input_path, loggly_key):
+    """
+    Main entrypoint to Spark
+    :param input_path: input file or path
+    :param loggly_key: loggly customer key for remote logging
+    :return: None
+    """
     spark = SparkSession.builder.appName("WordCount").getOrCreate()
 
     lines = spark.sparkContext.textFile(input_path).zipWithIndex()
-    parser = Parser(loggly_key)
-    counts = lines.flatMap(lambda x: parser.word_split(x)).map(lambda x: (x, 1)).reduceByKey(add)
+    txt_parser = Parser(loggly_key)
+    counts = lines.flatMap(lambda x: txt_parser.word_split(x)).map(lambda x: (x, 1)).reduceByKey(add)
     output = counts.sortBy(lambda x: x[1], ascending=False).collect()
 
     for (word, count) in output:
         print("%s: %i" % (word, count))
-
+    # configures logger in driver
     logger = get_configured_logger(loggly_key)
     logger.info("Wordcount complete")
     spark.stop()
